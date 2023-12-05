35bc31a7a0b963a68d99be12d6ef37ce512b42a4
Ali <ialimalik99@gmail.com>
2023-12-05 17:33:49 -0500

    add map function the list

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -3,15 +3,26 @@ import React from "react";
 const ProjectList = (props) => {
   return (
     <div>
-      <ul className="mt-8">
-        {props.projects.map((project) => (
-          <li
-            key={project.id}
-            className="flex justify-between my-4 text-white text-center"
-          >
-            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">{project.title}</button>
-          </li>
-        ))}
+      <ul className="m">
+        {props.projects.map((project) => {
+          let cssClasses = "w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-500"
+
+          if (project.id === props.selectedProjectId) {
+            cssClasses += ' bg-stone-800 text-stone-200'
+          } else {
+            cssClasses += ' text-stone-400'
+          }
+
+          // Corrected: Replaced curly braces with parentheses for an implicit return
+          return (
+            <li
+              key={project.id}
+              className="flex justify-between my-4 text-white text-center"
+            >
+              <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">{project.title}</button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
